c7fee392231999b645c03708e2213bccea4de794
zhengchun <zhengchun@yusys.com.cn>
2019-04-15 17:01:14 +0800

    加入启动robot的shell

--- DataUnload/robot.sh
+++ DataUnload/robot.sh
@@ -1,1 +1,1 @@
-robot.exe -d X:/Temp -v Y_M:2019-04 -v DATADIR:X:/Temp/201904 -t '下载项目投入明细' yusys_sy.robot
+robot.exe -d X:/Temp -v Y_M:2019-03 -v DATADIR:X:/Temp/201903 -t '下载项目投入明细' yusys_sy.robot
