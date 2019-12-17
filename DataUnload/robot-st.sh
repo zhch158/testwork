f208f8dfd558f8b8ff412c6e6e81460a231b3c9d
zhengchun <zhengchun@yusys.com.cn>
2019-12-17 20:17:56 +0800

    未修改

--- DataUnload/robot-st.sh
+++ DataUnload/robot-st.sh
@@ -1,4 +1,5 @@
 #robot.exe -d X:/Temp -v Y_M:2019-01 -v DATADIR:X:/Temp/201901 -t '下载项目投入明细' yusys_sy.robot
 #robot.exe -d X:/Temp -v Y_M:2019-01 -t '下载非项目费用明细-闲置、休假' yusys_sy.robot
-#robot.exe -d X:/Temp -v Y_M:2019-03 -t '下载考核、管理损益明细' yusys_sy.robot
-robot.exe -d X:/Temp -v Y_M:2019-04 -t '下载项目投入明细' yusys_sy.robot
+#robot.exe -d X:/Temp -v Y_M:2019-10 -t '下载考核、管理损益明细' yusys_sy.robot
+robot.exe -d X:/Temp -v Y_M:2019-11 -t '下载考核、管理损益汇总' yusys_sy.robot
+#robot.exe -d X:/Temp -v Y_M:2019-10 -t '下载项目投入明细' yusys_sy.robot
